e67415ee5a951fd9bed1acbf80b41e6a80d7a4b4
vimes <vimes@yandex-team.ru>
2024-10-31 16:36:04 +0300

    Adds bit-reverse unittest

---
+++ lib/unittest/test-bit-reverse.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "../bit-reverse.hpp"
+
+namespace
+{
+
+TEST(BitReverse, VisitsAll)
+{
+	static constexpr std::uint32_t Bits = 18;
+	static constexpr std::size_t Size = 1 << Bits;
+	std::vector<bool> seen(Size, false);
+
+		for (std::uint32_t i = 0, pos = 0; i < Size; ++i, pos = ReverseBits<Bits>(i))
+	{
+		ASSERT_FALSE(seen.at(pos));
+		seen[pos] = true;
+	}
+
+	ASSERT_TRUE(std::find(seen.begin(), seen.end(), false) == seen.end());
+}
+
+}
